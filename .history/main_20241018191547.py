37f2b11adedc632c9af9f69b1817b5c329710c50
Marta54 <msro1@live.com.pt>
2024-10-18 19:27:45 +0200

    pre-processed imgs

---
+++ .history/main_20241018191547.py
@@ -0,0 +1,17 @@
+from image_preprocess import make_square
+import matplotlib.pyplot as plt
+
+import glob
+import os
+
+# Specify the folder path
+folder_path = 'C:\\Users\\msro1\\Downloads\\archive (1)\\images'
+
+# Use glob to find all .jpg files recursively
+jpg_files = glob.glob(os.path.join(folder_path, '**', '*.jpg'), recursive=True)
+
+# Iterate through the found .jpg files
+for jpg_file in jpg_files:
+    # Perform the desired action on each .jpg file (e.g., resize, compress, etc.)
+    print(f"Found .jpg file: {jpg_file}")
+    # Replace this print statement with your desired action
---
+++ .history/main_20241018192507.py
@@ -0,0 +1,22 @@
+from image_preprocess import make_square
+import matplotlib.pyplot as plt
+
+import glob
+import os
+
+from PIL import Image
+
+# Specify the folder path
+folder_path = 'C:\\Users\\msro1\\Downloads\\archive (1)\\images'
+output_folder = ''
+# Use glob to find all .jpg files recursively
+jpg_files = glob.glob(os.path.join(folder_path, '**', '*.jpg'), recursive=True)
+
+# Iterate through the found .jpg files
+for jpg_file in jpg_files:
+    doc_file = jpg_file[jpg_file.rfind('\\'):]
+    print(doc_file)
+    # Perform the desired action on each .jpg file (e.g., resize, compress, etc.)
+    #make_square(jpg_file, 512).save(f'\pokemon_square\\{}')
+       # Replace this print statement with your desired action
+    break
