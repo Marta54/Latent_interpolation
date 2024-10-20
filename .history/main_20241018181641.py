0083a937d846beab1c2517bc83d429c371de9b71
Marta54 <msro1@live.com.pt>
2024-10-20 11:19:43 +0200

    clean by removing history and StyleGan

--- .history/main_20241018181641.py
+++
@@ -1,6 +0,0 @@
-from image_preprocess import make_square
-import matplotlib.pyplot as plt
-
-path = "C:\\Users\\msro1\\Downloads\\archive (1)\\images\\Zweilous\\1.jpg"
-img = make_square(path, size = 512)
-plt.imshow(img)
